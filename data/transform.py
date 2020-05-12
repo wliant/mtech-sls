7366bef9da23cbf96b05b6aec7aeb5605d18f998
Tan Wei Lian <twl_william89@hotmail.com>
2020-05-13 02:04:11 +0800

    possible data

---
+++ data/transform.py
@@ -0,0 +1,5 @@
+
+import csv
+src_headers = ["Date","Hour","BidOpen","BidHigh","BidLow","BidClose","BidChange","AskOpen","AskHigh","AskLow","AskClose","AskChange"]
+target_headers = ["Time","Open","High","Low","Close","Volume"]
+
